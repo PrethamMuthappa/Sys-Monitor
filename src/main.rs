f8d8a2c9b0b18b81a26285c722dcf141e81ee777
Pretham Muthappa <98420696+PrethamMuthappa@users.noreply.github.com>
2023-10-23 01:09:28 +0530

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-use std::thread::sleep;
-use eframe::{egui, HardwareAcceleration, Theme};
 use eframe::emath::Align;
-use egui::{Id, Sense, Vec2, RichText, Color32, Layout, FontId};
-
-
+use eframe::{egui, HardwareAcceleration, Theme};
+use egui::{Color32, FontId, Id, Layout, RichText, Sense, Vec2};
+use egui_plot::{Line, Plot, PlotPoints};
+use std::thread;
+use std::time::Duration;
+use systemstat::{Platform, System};
 
 fn main() {
-    let nativeoption=eframe::NativeOptions {
+    let nativeoption = eframe::NativeOptions {
         always_on_top: false,
         maximized: false,
         decorated: true,
@@ -14,7 +15,7 @@ fn main() {
         drag_and_drop_support: true,
         icon_data: None,
         initial_window_pos: None,
-        initial_window_size: Option::from(Vec2::new(850f32,650f32)),
+        initial_window_size: Option::from(Vec2::new(850f32, 650f32)),
         min_window_size: None,
         max_window_size: None,
         resizable: true,
@@ -39,66 +40,74 @@ fn main() {
     };
     eframe::run_native(
         "resource-monitor",
-        nativeoption,Box::new(|cc| Box::new(Res::new(cc))),
+        nativeoption,
+        Box::new(|cc| Box::new(Res::new(cc))),
     )
-        .expect("error")
+    .expect("error")
 }
 
 #[derive(Default)]
-struct Res {
-    name:String,
-}
+struct Res {}
 
 impl Res {
-    fn new (_cc:&eframe::CreationContext<'_>) -> Self {
-
-   Self::default()
-
-}
-
-    fn data(&self) {}
-
-
+    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+        Self::default()
+    }
 }
 
 impl eframe::App for Res {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            egui::ScrollArea::vertical().show(ui, |ui| {
+                if ui
+                    .interact(ui.max_rect(), Id::new("window-drag"), Sense::drag())
+                    .dragged()
+                {
+                    frame.drag_window();
+                }
 
-fn update(&mut self, ctx:&egui::Context, frame:&mut eframe::Frame) {
+                ui.with_layout(Layout::top_down(Align::Center), |ui| {
+                    ui.heading(
+                        RichText::new("Resource monitor")
+                            .color(Color32::RED)
+                            .font(FontId::monospace(28.5)),
+                    );
+                });
 
-egui::CentralPanel::default().show(ctx,|ui| {
-   
+                ui.separator();
 
-egui::ScrollArea::vertical().show(ui,|ui| { 
+                ui.label(
+                    RichText::new("CPU")
+                        .color(Color32::GREEN)
+                        .font(FontId::monospace(20.0)),
+                );
 
-if ui.interact(ui.max_rect(), Id::new("window-drag"),Sense::drag()).dragged(){ 
-          frame.drag_window();
- }
+                let sys = System::new();
 
-    ui.with_layout(Layout::top_down(Align::Center), |ui| {
-        ui.heading(RichText::new("Resource monitor").color(Color32::RED).font(FontId::monospace(28.5)));
-    });
+                match sys.cpu_load_aggregate() {
+                    Ok(cpu) => {
+                        println!("\n measuring cpu load");
+                        thread::sleep(Duration::from_secs(1));
+                        let cpu = cpu.done().unwrap();
+                        println!("{:?}", cpu.user);
+                    }
+                    Err(error) => println!("{:?}", error),
+                }
 
+                ui.separator();
 
-    ui.separator();
+                let sin: PlotPoints = (0..10)
+                    .map(|i| {
+                        let x = i as f64 * 0.01;
+                        [x, x.sin()]
+                    })
+                    .collect();
 
-    ui.label(RichText::new("CPU").color(Color32::GREEN).font(FontId::monospace(20.0)));
-
-    if ui.button("click").clicked() {
-    self.name="pretham".to_string();
+                let line = Line::new(sin);
+                Plot::new("my plot")
+                    .view_aspect(2.0)
+                    .show(ui, |plot_ui| plot_ui.line(line));
+            });
+        });
     }
-
-
-    if ui.button("clear").clicked() {
-        self.name.clear();
-    }
-
-    ui.label(&self.name);
-
- });
- }); 
-
-
-
-}
-
 }
